4c1624eaea71b5d6c578218f56b0f5514e65aa39
hazichomwarr <batchiy6@gmail.com>
2026-03-08 17:51:40 -0400

    feat: add new-request page

---
+++ app/new-request/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/ui/Container";
+
+export default function newRequestPage() {
+  return (
+    <section className="bg-neutral-50">
+      <Container>Coming Soon...</Container>
+    </section>
+  );
+}
